6e31edbfc90a1ec3089cc601435b332456c41a0c
JetJadeja <jetjadeja@gmail.com>
2025-07-10 15:38:40 -0700

    update base rainbow test to use v4

--- test/BaseRainbowTest.t.sol
+++ test/BaseRainbowTest.t.sol
@@ -2,26 +2,24 @@
 pragma solidity ^0.8.0;
 
 import { console } from "forge-std/console.sol";
-
-import { ISwapRouter } from "vendor/v3-periphery/interfaces/ISwapRouter.sol";
-import { ISwapRouter02 } from "vendor/swap-router/interfaces/ISwapRouter02.sol";
 import { Test } from "forge-std/Test.sol";
-import { SwapRouter02, IV3SwapRouter } from "vendor/swap-router/SwapRouter02.sol";
-import { SwapRouter } from "vendor/v3-periphery/SwapRouter.sol";
-import { NonfungiblePositionManager } from "vendor/v3-periphery/NonfungiblePositionManager.sol";
-import { UniswapV3Factory } from "vendor/v3-core/UniswapV3Factory.sol";
-import { WETH } from "test/mocks/WETH.sol";
-import { RainbowSuperToken } from "src/RainbowSuperToken.sol";
+import { IWETH9 } from "lib/v4-periphery/src/interfaces/external/IWETH9.sol";
+import { IPoolManager } from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import { IPositionManager } from "lib/v4-periphery/src/interfaces/IPositionManager.sol";
+import { IUniversalRouter } from "vendor/universal-router/IUniversalRouter.sol";
 import { RainbowSuperTokenFactory } from "src/RainbowSuperTokenFactory.sol";
 
 contract BaseRainbowTest is Test {
-    // Core contracts
-    NonfungiblePositionManager public nftPositionManager;
-    UniswapV3Factory public factory;
-    WETH public weth;
-    SwapRouter02 public swapRouter;
+    // V4 contracts (mainnet addresses)
+    IPoolManager public poolManager = IPoolManager(0x000000000004444c5dc75cB358380D2e3dE08A90);
+    IPositionManager public positionManager = IPositionManager(0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e);
+    IUniversalRouter public universalRouter = IUniversalRouter(0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af);
+    address public permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    IWETH9 public weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    
+    // Our factory contract
     RainbowSuperTokenFactory public rainbowFactory;
-
+    
     // Test accounts
     address public owner = makeAddr("owner");
     address public creator1 = makeAddr("creator1");
@@ -33,32 +31,25 @@ contract BaseRainbowTest is Test {
     // Constants for testing
     uint256 constant INITIAL_SUPPLY = 10 ether;
     uint24 constant POOL_FEE = 10_000; // 1%
-
+    
     function setUp() public virtual {
-        vm.startPrank(owner);
-
-        uint256 id;
-        assembly {
-            id := chainid()
-        }
-
-        // Deploy Uniswap V3 contracts
-        factory = new UniswapV3Factory();
-        weth = new WETH();
-
-        // Deploy position manager
-        nftPositionManager = new NonfungiblePositionManager(
-            address(factory),
-            address(weth),
-            address(0) // No descriptor needed for testing
-        );
-
-        swapRouter = new SwapRouter02(address(0), address(factory), address(nftPositionManager), address(weth));
-
-        // Deploy Rainbow factory
+        // Fork mainnet at a recent block
+        vm.createSelectFork(vm.envString("ETH_RPC_URL"), 18900000);
+        
+        // Deploy only your factory
+        vm.prank(owner);
         rainbowFactory = new RainbowSuperTokenFactory(
-            address(factory), address(pot), address(nftPositionManager), address(swapRouter), address(weth), "https://rainbow.me/tokens"
+            address(poolManager),
+            address(positionManager),
+            address(universalRouter),
+            address(permit2),
+            address(pot),
+            address(weth),
+            "https://rainbow.me/tokens"
         );
-        vm.stopPrank();
+        
+        // Fund test accounts
+        vm.deal(creator1, 100 ether);
+        vm.deal(user1, 100 ether);
     }
 }
